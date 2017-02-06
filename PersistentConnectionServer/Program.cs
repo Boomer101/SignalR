9fb34b72e3a6e9bffe8bb8a6e8307cb9e671e68f
Adrian Bachmann <boomerhimself@gmx.ch>
2017-02-06 11:26:54 +0100

    Init EADN SignalR

---
+++ PersistentConnectionServer/Program.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
+using Owin;
+using Microsoft.Owin.Cors;
+
+namespace PersistentConnectionServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "http://localhost:8080";
+            using (WebApp.Start<Startup>(url))
+            {
+                Console.WriteLine($"Server is running on {url}");
+                Console.ReadLine();
+            }
+        }
+    }
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Cors zuerst, Reihenfolge wichtig !
+            app.UseCors(CorsOptions.AllowAll);
+            app.MapSignalR<MyPersistentConnection>("/ps");
+        }
+    }
+
+    public class MyPersistentConnection : PersistentConnection
+    {
+        protected override Task OnConnected(IRequest request, string connectionId)
+        {
+            // Jeder Client erhält eine eigene ID, pro neue Verbindung 
+            Console.WriteLine($"New client connected, Connection ID: {connectionId}");
+            return base.OnConnected(request, connectionId);
+        }
+        protected override async Task OnReceived(IRequest request, string connectionId, string data)
+        {
+            // Antwort an alle Clients
+            await Connection.Broadcast(data);
+        }
+    }
+}
---
+++ SignalR/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Owin.Host.HttpListener;
+using Microsoft.Owin.Hosting;
+using System.IO;
+
+namespace OwinSample
+{
+    using Owin;
+    using AppFunc = Func<IDictionary<string, object>, Task>;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "http://localhost:8080";
+            using (WebApp.Start<Startup>(url))
+            {
+                Console.WriteLine($"Server started on {url}");
+                Console.ReadKey();
+            }
+        }
+    }
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //app.UseWelcomePage();
+            app.Use<HelloEadnMiddleware>();
+        }
+    }
+
+    public class HelloEadnMiddleware
+    {
+        public HelloEadnMiddleware(AppFunc context)
+        {
+
+        }
+        public async Task Invoke(IDictionary<string, object> environment)
+        {
+            var response = environment["owin.ResponseBody"] as Stream;
+            using(StreamWriter writer = new StreamWriter(response))
+            {
+                await writer.WriteAsync("Hello EADN 2");
+            }
+        }
+    }
+
+    public class LoggerModule
+    {
+        public LoggerModule() { }
+
+    }
+}
